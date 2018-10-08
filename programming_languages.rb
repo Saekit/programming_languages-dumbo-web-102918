9abef18fdada4500ee3c38c5ac12e94aed6983d6
Raquel Randall <raquel.sae.randall@gmail.com>
2018-10-08 17:57:51 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,9 +2,10 @@ def reformat_languages(languages)
   new_hash = {}
   styles_array = []
   languages.each do |style, value|
-    styles_array << style 
+    styles_array << style
     value.each do |language, type|
-
-
+      new_hash[language] = {type, style}
+    end
+  end
   new_hash
 end
